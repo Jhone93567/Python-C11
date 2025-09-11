833d32411b17a59c7a4cfbc30b695afd0fcdf919
Jhone93567 <joaogabrielchereze@hotmail.com>
2025-09-11 20:25:56 -0300

    Aula do dia 11/09

--- Aula 5 Introducao ao Pandas/exemplos.py
+++ Aula 5 Introducao ao Pandas/exemplos.py
@@ -37,3 +37,47 @@ print(df.loc[['A', 'B'], ['W', 'X', 'Y', 'Z']])
 
 # Imprime so as colunas W e Y
 print(df[['W','Y']])
+
+# Importando datasets com Pandas
+ds = pd.read_csv('paises.csv', delimiter=';')
+print(ds)
+
+# Imprimindo so o nome das colunas
+print(ds.columns)
+
+# Calculando a porcentagem da populacao de cada pais em relacao ao resto
+somaPopulacao = np.sum(ds['Population'])
+print(somaPopulacao)
+
+populationPerc = ds['Population']/somaPopulacao
+print(populationPerc)
+
+# Adicionando uma nova coluna no dataset
+ds['PopulationPercent'] = populationPerc
+print(ds)
+
+# Exportando a nova versao do dataset
+ds.to_csv('paises2.csv', sep=';')
+
+# Agrupar os dados do dataset por regiao
+group_region = ds.groupby('Region')
+print(group_region.count()['Country'])
+
+# Funcoes customizadas no pandas
+def terpercent(x):
+    return x*0.9
+
+# Pegando a taxa de mortalidade do dataset
+taxaMort = ds['Deathrate']
+
+# Criando uma series descontada 10% da taxa de mortalidade
+taxaMort2 = taxaMort.apply(terpercent)
+print(taxaMort2)
+
+# Criando um novo dataframe apenas com estas duas series
+df2 = pd.concat([taxaMort, taxaMort2], axis=1)
+df2.columns = ['Taxa de Mortalidade', 'Taxa de Mortalidade com Desconto'] # Isso muda o nome das colunas
+print(df2)
+
+# Removendo linhas com dados ausentes (remove a linha toda mesmo se faltar so uma celula)
+novoDataFrame = df2.dropna()
